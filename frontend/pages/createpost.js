eaa708f2243acca0398daf346dcbb98da1b1803a
Niteshkumar <nkumar35101@gmail.com>
2022-12-21 19:43:47 +0530

    createpost card created

---
+++ frontend/pages/createpost.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Createpost from '../components/Createpost'
+
+const createpost = () => {
+  return (
+    <div><Createpost/></div>
+  )
+}
+
+export default createpost
